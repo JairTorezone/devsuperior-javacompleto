a844bd1004eaa702bcbee307bf8a552ee716000a
Jair Torezone <jairtorezone@gmail.com>
2021-11-01 21:35:22 -0300

    estrutura ternaria

--- src/com/devsuperior/m5/estruturacondicional/CondicaoTernaria.java
+++ src/com/devsuperior/m5/estruturacondicional/CondicaoTernaria.java
@@ -3,7 +3,7 @@ package com.devsuperior.m5.estruturacondicional;
 public class CondicaoTernaria {
 
 	public static void main(String[] args) {
-		
+		//Estrugura ternaria
 		double preco = 34.5;
 		
 		double desconto = (preco < 20) ? preco * 0.1 : preco * 0.05;
