d4b855b189ef10d3b896cb5bc07aaafae822d75c
Jair Torezone <jairtorezone@gmail.com>
2021-12-29 09:47:16 -0300

    Generics - Mostrar o primeiro elemento lista

---
+++ src/com/devsuperior/m19/genericsMaps/generics/application/Program.java
@@ -0,0 +1,29 @@
+package com.devsuperior.m19.genericsMaps.generics.application;
+
+import java.util.Scanner;
+
+import com.devsuperior.m19.genericsMaps.generics.services.PrintService;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		PrintService<String> ps = new PrintService<>();
+		
+		System.out.print("How many values? ");
+		int n = sc.nextInt();
+		
+		for (int i = 0; i < n; i++) {
+			String value = sc.next();
+			ps.addValue(value);
+		}
+
+		ps.print();
+		String x = ps.first();
+		System.out.println("First: " + x);
+		
+		sc.close();
+	}
+}
