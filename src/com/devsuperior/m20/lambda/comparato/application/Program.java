c4167a1603fc04d63881d6f93cc14f5c1e2bc3e5
Jair Torezone <jairtorezone@gmail.com>
2022-02-03 20:30:51 -0300

    Lambda - Class anonymous (sem lambda)

--- src/com/devsuperior/m20/lambda/comparato/application/Program.java
+++ src/com/devsuperior/m20/lambda/comparato/application/Program.java
@@ -1,6 +1,7 @@
 package com.devsuperior.m20.lambda.comparato.application;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import com.devsuperior.m20.lambda.comparato.entities.Produto;
@@ -8,15 +9,29 @@ import com.devsuperior.m20.lambda.comparato.entities.Produto;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		List<Produto> list = new ArrayList<>();
-		
+
 		list.add(new Produto("TV", 900.00));
 		list.add(new Produto("Notebook", 1200.00));
 		list.add(new Produto("Iphone", 1900.00));
 		
-		list.sort(new MyComparator());
 		
+//--------------------Classe separada implementando a interface--------------
+//		list.sort(new MyComparator());
+
+//------------------- Class Anonymous-----------------------------------------
+//		Comparator<Produto> comp = new Comparator<Produto>() {
+//			@Override
+//			public int compare(Produto p1, Produto p2) {
+//				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+//			}
+//		};
+//		
+//		list.sort(comp);
+		
+		
+
 		for (Produto p : list) {
 			System.out.println(p);
 		}
