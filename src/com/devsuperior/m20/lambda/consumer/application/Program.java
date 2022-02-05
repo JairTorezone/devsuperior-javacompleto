bfcd151fc4105691145fd5f28618e136049d1041
Jair Torezone <jairtorezone@gmail.com>
2022-02-05 12:44:42 -0300

    Consumer - Expressão lambda declarada

--- src/com/devsuperior/m20/lambda/consumer/application/Program.java
+++ src/com/devsuperior/m20/lambda/consumer/application/Program.java
@@ -3,6 +3,7 @@ package com.devsuperior.m20.lambda.consumer.application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 import com.devsuperior.m20.lambda.consumer.util.PriceUpdate;
 import com.devsuperior.m20.lambda.consumer.entities.Product;
@@ -19,7 +20,15 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.forEach(Product::nonStaticPriceUpdate);
+		double value = 1.1;
+		
+		Consumer<Product> cons = p -> {
+			p.setPrice(p.getPrice() * value);
+		};
+		//ou 
+		//Consumer<Product> cons = p -> p.setPrice(p.getPrice() * value);
+		
+		list.forEach(cons);
 		
 		list.forEach(System.out::println);
 		
