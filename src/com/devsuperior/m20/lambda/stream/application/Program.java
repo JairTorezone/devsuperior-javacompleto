7cfa7e93c40afceced8babc0b7b9f5843fcf061e
Jair Torezone <jairtorezone@gmail.com>
2022-02-18 20:01:20 -0300

    Stream - map e toArray)

---
+++ src/com/devsuperior/m20/lambda/stream/application/Program.java
@@ -0,0 +1,16 @@
+package com.devsuperior.m20.lambda.stream.application;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
+		Stream<Integer> st1 = list.stream().map(x -> x * 10); // map - Aplica uma função a cada elemento da stream
+		System.out.println(Arrays.toString(st1.toArray())); // toArray converte a stream para um vetor
+	}
+
+}
