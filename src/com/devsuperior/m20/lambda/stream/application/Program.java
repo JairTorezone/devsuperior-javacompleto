5691c8a042e4e4382b3918296b7e3b2fa0545efa
Jair Torezone <jairtorezone@gmail.com>
2022-02-18 20:05:38 -0300

    Stream - criando streams com Stream.of

--- src/com/devsuperior/m20/lambda/stream/application/Program.java
+++ src/com/devsuperior/m20/lambda/stream/application/Program.java
@@ -11,6 +11,11 @@ public class Program {
 		List<Integer> list = Arrays.asList(3, 4, 5, 10, 7);
 		Stream<Integer> st1 = list.stream().map(x -> x * 10); // map - Aplica uma função a cada elemento da stream
 		System.out.println(Arrays.toString(st1.toArray())); // toArray converte a stream para um vetor
+		
+		Stream<String> st2 = Stream.of("Maria", "Alex", "Bob");
+		System.out.println(Arrays.toString(st2.toArray()));
+		
+		
 	}
 
 }
